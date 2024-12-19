c771e2a97b115b8f56cb19d2203ab9bb63a05d78
duyenpham95 <duyenpham.june.1995@gmail.com>
2024-12-19 15:36:02 +1100

    first commit

---
+++ HuliPizzaProject/HuliPizzaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaProjectApp.swift
+//  HuliPizzaProject
+//
+//  Created by Duyen on 19/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
